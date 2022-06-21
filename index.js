45d695561580b5fde885b2a0765b55e8f6533ad9
kendall baker <d00262562@dmail.dixie.edu>
2022-06-21 09:02:51 -0600

    added data members

---
+++ index.js
@@ -0,0 +1,84 @@
+// initiate express server
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+// pull in db
+const persist = require("./persist");
+
+// put in command line flags
+const flags = require("flags");
+flags.defineNumber("port", 3000, "Ports for the http servier to listen on");
+flags.parse();
+
+// put in env vars
+const dotenv = require("dotenv");
+
+// set up port number
+const port = flags.get("port") || process.env.PORT || 3000;
+
+// set up server paths and handlers
+app.get("/todo/:id", (req, res) => {
+    const id = req.params.id;
+    const todo = persist.getTodo(id);
+    res.json(todo);
+});
+
+app.get("/todos", (req, res) => {
+    res.json(persist.getTodos());
+});
+
+app.post("/todo", (req, res) => {
+    // validate the data
+    const vTodo = setupTodo(req.body);
+    const todo = persist.addTodo(vTodo);
+    res.json(todo);
+});
+
+app.delete("/todo/:id", (req, res) => {
+    const id = req.params.id;
+    const todo = persist.deleteTodo(id);
+    res.json(todo);
+});
+
+app.put("/todo/:id", (req, res) => {
+    const id = req.params.id;
+    // validate the data
+    const vTodo = setupTodo(req.body);
+    const todo = persist.setTodo(id, vTodo);
+    res.json(todo);
+});
+
+app.patch("/todo/:id", (req, res) => {
+    const id = req.params.id;
+    // validate the data
+    // this validation is done a bit different than the post and put
+    const todo = persist.patchTodo(id, req.body);
+    res.json(todo);
+});
+
+// start server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+setupTodo = function (todoData) {
+    let deadline = new Date();
+    let done = false;
+    // check deadline and make sure its good
+    if (todoData.deadline) {
+        deadline = new Date(todoData.deadline);
+    }
+    // check done and make sure its good
+    if (todoData.done) {
+        done = todoData.done;
+    }
+    // set defaults for eveything else
+    return {
+        name: todoData.name || "",
+        description: todoData.description || "",
+        done: done,
+        deadline: deadline,
+    };
+};
---
+++ persist/index.js
@@ -0,0 +1,64 @@
+const todo_db = {};
+
+function makeid(length) {
+    var result = "";
+    var characters =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    var charactersLength = characters.length;
+    for (var i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
+
+const addTodo = function (todo) {
+    id = makeid(8);
+    todo.id = id;
+    todo_db[id] = todo;
+    console.log(todo_db);
+    return todo;
+};
+
+const getTodo = function (id) {
+    return todo_db[id];
+};
+
+const getTodos = function () {
+    return todo_db;
+};
+
+const setTodo = function (id, todo) {
+    todo_db[id] = todo;
+    return todo;
+};
+
+// patchTodo - update a todo
+const patchTodo = function (id, todoData) {
+    // loop over the data and set each individual item
+    for (const key in todoData) {
+        todo_db[id][key] = todoData[key];
+    }
+
+    // Object.keys(todoData).forEach(function (key) {
+    //   todo_db[id][key] = todoData[key];
+    // });
+
+    // pull the new data
+    const todo = todo_db[id];
+    return todo;
+};
+
+const deleteTodo = function (id) {
+    const todo = todo_db[id];
+    delete todo_db[id];
+    return todo;
+};
+
+module.exports = {
+    addTodo: addTodo,
+    getTodo: getTodo,
+    getTodos: getTodos,
+    setTodo: setTodo,
+    patchTodo: patchTodo,
+    deleteTodo: deleteTodo,
+};
